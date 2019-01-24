97eeb498718ace067b7ed4e7de463762cc9d8bf8
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-01-24 12:03:20 +0100

    Created and tested function save_ascii with a hello world string

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
-int main(int argc, char *argv) {
-  printf("Hello world!\n");
+void save_ascii(unsigned char *color, int N, int M) {
+  FILE *fp;
+
+  fp = fopen("color.txt", "w");
+  for (int j = 0; j < N; j++) {
+    for (int i = 0; i < M; i++) {
+      fprintf(fp, "%hhu ", color[i + j*M]);
+    }
+    fprintf(fp, "\n");
+  }
+  fclose(fp);
 }
+
+int main(int argc, char *argv) {
+  unsigned char *demo;
+
+  demo = (unsigned char *)"Hello world!\n";
+  save_ascii(demo, 14, 1);
+}
+
+
