d770fdb209f6a82acd333982ed27a1beae47a6a0
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-02-07 12:33:44 +0100

    Last modifications to the code. Format of -r argument is "-r rx ry rw rh" in the natural way (0,0 in the top left point when visualizing in Matlab)

--- mandelbrot.c
+++ mandelbrot.c
@@ -4,6 +4,7 @@
 #include <mpi.h>
 #include <math.h>
 #include <complex.h>
+#include <unistd.h>
 
 #define TAG 123
 
@@ -14,7 +15,7 @@ void save_ascii(unsigned char *color, int N, int M) {
   fp = fopen("color.txt", "w");
   for (j = 0; j < N; j++) {
     for (i = 0; i < M; i++) {
-      fprintf(fp, "%hhu ", color[i + j*M]);
+      fprintf(fp, "%hhu ", color[j + i*N]);
     }
     fprintf(fp, "\n");
   }
@@ -54,19 +55,16 @@ void slave(int p, int q, int hp, int w, double dy, double dx, int b, int N, doub
   double yoff = 0.0;
   double x_step = rh / hp;
   double y_step = rw / w;
-  int ix = 0, iy = 0;
-  double x, y;
-  for (x = rx; x < rx + rh; x+=x_step) {
-    iy = 0;
-    dreal = (x + xoff) * dx - b;
-    for (y = ry; y < ry + rw; y+=y_step) {
-      dimag = (y + yoff) * dy - b;
+  int ix, iy;
+  for (ix = 0; ix < hp; ix++) {
+    dreal = (rx + ix*x_step + xoff) * dx - b;
+    for (iy = 0; iy < w; iy++) {
+      dimag = (ry + iy*y_step + yoff) * dy - b;
       d = dreal + dimag * I;
       buf[ix * w + iy] = cal_pixel(d, b, N);
-      iy++;
     }
-    ix++;
-  }
+   }
+  printf("x: %d\ty: %d\t\n", ix*3, iy);
   int rc = MPI_Send((void *)buf, w * hp, MPI_UNSIGNED_CHAR, 0, TAG, MPI_COMM_WORLD);
   /* printf("Return code from send/rank %d: %d\n", q+1, rc); */
   /* printf("Everything sent from rank %d\n", q + 1); */
@@ -75,28 +73,37 @@ void slave(int p, int q, int hp, int w, double dy, double dx, int b, int N, doub
 int main(int argc, char *argv[]) {
   int myrank, numprocs;
 
+  int i = 0;
   int w = 2048;
   int h = 2048;
+  while (i < argc) {
+    if (strcmp(argv[i], "-w") == 0) {
+      w = atoi(argv[i+1]);
+      i += 1;
+    } else if (strcmp(argv[i], "-h") == 0) {
+      h = atoi(argv[i+1]);
+      i += 1;
+    }
+    i++;
+  }
+
   int N = 256;
   double b = 1;
   double rx = 0, ry = 0, rw = w, rh = h;
 
-  int i = 1;
+  i = 0;
   while (i < argc) {
-    if (strcmp(argv[i], "-w") == 0) {
-      w = atoi(argv[i+1]);
-      i += 2;
-    } else if (strcmp(argv[i], "-h") == 0) {
-      h = atoi(argv[i+1]);
-      i += 2;
-    } else if (strcmp(argv[i], "-r") == 0) {
+    if (strcmp(argv[i], "-r") == 0) {
       rx = atof(argv[i+1]);
       ry = atof(argv[i+2]);
       rh = atof(argv[i+3]);
       rw = atof(argv[i+4]);
-      i += 5;
+      i += 4;
     }
+    i++;
   }
+
+  i = 1;
 
   MPI_Init(&argc, &argv);
 
@@ -106,6 +113,7 @@ int main(int argc, char *argv[]) {
   double dy = 2.0 * b / (w - 1);
   double dx = 2.0 * b / (h - 1);
   int hp = h / (numprocs - 1);
+  double rhp = rh / (numprocs - 1);
   
   printf("My rank is %d\n", myrank);
   switch (myrank) {
@@ -113,7 +121,7 @@ int main(int argc, char *argv[]) {
     master(w, h, hp, numprocs);
     break;
   default:
-    slave(0, myrank - 1, hp, w, dy, dx, b, N, rx, ry, rh, rw);
+    slave(0, myrank - 1, hp, w, dy, dx, b, N, rx, ry, rhp, rw);
     break;
   }
 
